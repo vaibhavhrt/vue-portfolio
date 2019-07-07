f5dd64e0d9013b29f92acade0a76a3081c6a0576
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-07-07 19:04:20 +0530

    Revert "Updates"
    This reverts commit 4a24c5c20e792af1cb60bcb6f5baf98de1a3a74e.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <main>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 32;
+  display: flex;
+}
+main {
+  width: 80%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import App from './App.vue'
+import Hello from './components/HelloWorld.vue'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+const routes = [
+  { path: '/', component: Hello },
+  // { path: '/projects/', component: Projects },
+  // { path: '/contact/', component: Contact },
+  // { path: '/resume/', component: Resume },
+]
+
+const router = new VueRouter({ routes })
+
+new Vue({
+  render: h => h(App),
+  router,
+}).$mount('#app')

a4f8bcdc77ac73b6b2479b691b3f124085960c22
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-07-08 00:45:49 +0530

    add resume page, using workaround for resume file download

---
+++ src/components/Resume.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="root">
+    <a
+      v-bind:href="`${publicPath}resume.pdf`"
+      target='_blank'
+      rel='noopener noreferrer'
+      download='Vaibhav Vishal Resume.pdf'
+    >
+      Download Résumé as pdf
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Resume",
+  data: function () {
+    return {
+      publicPath: process.env.BASE_URL,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding: 32px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import Hello from './components/Hello.vue'
 import Projects from './components/Projects.vue'
 import Contact from './components/Contact.vue'
+import Resume from './components/Resume.vue'
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
@@ -13,7 +14,7 @@ const routes = [
   { path: '/', component: Hello },
   { path: '/projects/', component: Projects },
   { path: '/contact/', component: Contact },
-  // { path: '/resume/', component: Resume },
+  { path: '/resume/', component: Resume },
 ]
 
 const router = new VueRouter({ routes })
